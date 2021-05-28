346b87969fd5723a6b9b3baf103e9b21dbdf5699
Shilohjones194 <76697212+Shilohjones194@users.noreply.github.com>
2021-05-28 16:43:09 -0700

    fixed bug!

--- server.js
+++ server.js
@@ -1,28 +1,18 @@
 const express = require('express');
-//Testing from Module 11
-// const { database } = require('./db/db.json');
-//const fs = require('fs');
+
 const apiRoutes = require('./routes/apiRoutes');
 const htmlRoutes = require('./routes/htmlRoutes');
 const PORT = process.env.PORT || 3001;
 const app = express();
-//Testing from module 11
-// app.get('/api/db/db.json', (req, res) => {
-//     res.json(database);
-// });
-​
-//middle ware
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
-//app.use(require('./routes/apiRoutes.js));
-//Need to require routes files
-// require('./routes/apiRoutes.js')(app);
-// require('./routes/htmlRoutes')(app);
+
 app.use('/api', apiRoutes);
 app.use('/', htmlRoutes);
-​
-// Setup listener
+
+
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
 });
